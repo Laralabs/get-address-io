e88c6d1a244c4eacf5e57d9bf1e1b9f50e9bfa1c
Matt Clinton <matt@mattc.dev>
2024-02-12 21:36:45 +0000

    Add test for responseToCache() method

--- tests/Unit/Cache/ManagerTest.php
+++ tests/Unit/Cache/ManagerTest.php
@@ -5,6 +5,7 @@ namespace Laralabs\GetAddress\Tests\Unit\Cache;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laralabs\GetAddress\Cache\Manager;
 use Laralabs\GetAddress\Models\CachedAddress;
+use Laralabs\GetAddress\Tests\Support\Responses\ResponseFactory;
 use Laralabs\GetAddress\Tests\TestCase;
 
 class ManagerTest extends TestCase
@@ -44,5 +45,30 @@ class ManagerTest extends TestCase
         $this->assertCount(1, $result['addresses'] ?? 0);
         $this->assertEquals('1 Example Street', $result['addresses'][0]['line_1']);
         $this->assertEquals('ABC 123', $result['postcode']);
+    }
+
+    /** @test */
+    public function it_can_store_the_address_collection_response_to_cache(): void
+    {
+        $response = ResponseFactory::make('successfulFindResponse.json')
+            ->makeAddressCollectionResponse();
+        $expectedAddress = $response->getAddresses()[0];
+
+        $this->assertEquals(0, CachedAddress::count());
+
+        $this->manager->responseToCache($response);
+
+        $this->assertEquals(14, CachedAddress::count());
+        $this->assertEquals($expectedAddress->getLine1(), CachedAddress::first()->line_1);
+        $this->assertEquals($expectedAddress->getLine2(), CachedAddress::first()->line_2);
+        $this->assertEquals($expectedAddress->getLine3(), CachedAddress::first()->line_3);
+        $this->assertEquals($expectedAddress->getLine4(), CachedAddress::first()->line_4);
+        $this->assertEquals($expectedAddress->getLocality(), CachedAddress::first()->locality);
+        $this->assertEquals($expectedAddress->getTown(), CachedAddress::first()->town_or_city);
+        $this->assertEquals($expectedAddress->getCity(), CachedAddress::first()->town_or_city);
+        $this->assertEquals($expectedAddress->getCounty(), CachedAddress::first()->county);
+        $this->assertEquals($response->getPostcode(), CachedAddress::first()->postcode);
+        $this->assertIsFloat(CachedAddress::first()->longitude);
+        $this->assertIsFloat(CachedAddress::first()->latitude);
     }
 }
